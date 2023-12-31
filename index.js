6e767e1037605fe248fbb5a0ab7871d8a55e8968
Ido Bosmi <ido.bosmi@gmail.com>
2023-12-31 12:06:19 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+const axios = require('axios');
+
+
+// Load your Telegram API token from the .env file
+const token = process.env.TELEGRAM_TOKEN;
+
+// Load your TrueCaller API token from the .env file
+const trueCallerToken = process.env.TRUE_CALLER_TOKEN;
+
+// Create a bot using the token
+const bot = new TelegramBot(token, {polling: true});
+
+// Listen for incoming messages
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const phoneNumber = msg.text;
+
+  
+  try {
+    const truecallerResponse = await getTruecallerInfo(phoneNumber);
+    
+    const name = truecallerResponse.name;
+    const countryCode = truecallerResponse.phones[0].countryCode;
+
+    // Format message
+    const message = `Hey! Thanks for using *Who is this*\n\nThe person you are looking for is:\n\n *${name}*\n\n from:\n *${countryCode}*`;
+
+    bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+  } catch (error) {
+    bot.sendMessage(chatId, `Error fetching Truecaller info: ${error.message}`);
+  }
+});
+
+
+// Function to make a request to Truecaller API
+async function getTruecallerInfo(phoneNumber) {
+  const url = 'https://asia-south1-truecaller-web.cloudfunctions.net/api/noneu/search/v1';
+  const params = {
+    q: phoneNumber,
+    countryCode: 'il',
+    type: '40',
+  };
+
+  const headers = {
+    Authorization: `Bearer ${trueCallerToken}`,
+  };
+
+  const response = await axios.get(url, { params, headers });
+  return response.data;
+}
+
+console.log('Bot is running...');
